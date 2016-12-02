12d46dcbeab69f2fe60d1ec418b4d385582af8e6
abdi1234 <abdillea@uni.coventry.ac.uk>
2016-12-02 17:28:48 +0000

    Update Week 3 Task 3.py

--- Week 3 Task 3.py
+++ Week 3 Task 3.py
@@ -3,9 +3,9 @@ def RemoveVowels(str):
     if not str:
         return ""
     if str[0] in ['a','e','i','o','u','A','E','I','O','U']:
-        return RemoveVowels(str[1:])
+        return RemoveVowels(str[1:]) #it recursively looks at the rest of the string
     else :
-        return str [0] + RemoveVowels(str[1:])
+        return str [0] + RemoveVowels(str[1:]) #it adds the character and recursively looks at the rest of the string
 
 
     
